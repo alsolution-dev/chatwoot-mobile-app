25cc271ecebb219b7da5f2c28dbadf283244086e
alsolution-dev <notifica@alsolution.net.br>
2026-01-21 19:51:28 -0300

    Refactor Google Services file path in app config

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,9 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
+import path from 'path';
 
 export default ({ config }: ConfigContext): ExpoConfig => {
+  const googlePath = path.resolve(__dirname, 'google-services.json');
+
   return {
     name: 'AL-Solution Chat',
     slug: 'alsolution-chat',
@@ -14,42 +17,14 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       image: './assets/splash.png',
       resizeMode: 'contain',
       backgroundColor: '#ffffff',
-      enableFullScreenImage_legacy: true,
     },
     ios: {
-      supportsTablet: true,
       bundleIdentifier: 'br.net.alsolution.chatwoot',
-      infoPlist: {
-        NSCameraUsageDescription: 'This app requires access to the camera to upload images and videos.',
-        NSPhotoLibraryUsageDescription: 'This app requires access to the photo library to upload images.',
-        NSMicrophoneUsageDescription: 'This app requires access to the microphone to record audio.',
-        UIBackgroundModes: ['fetch', 'remote-notification'],
-        ITSAppUsesNonExemptEncryption: false,
-      },
-      googleServicesFile: './google-services.json',
-      entitlements: { 'aps-environment': 'production' },
-      associatedDomains: ['applinks:saas.alsolution.net.br'],
+      googleServicesFile: googlePath,
     },
     android: {
-      adaptiveIcon: { foregroundImage: './assets/adaptive-icon.png', backgroundColor: '#ffffff' },
       package: 'br.net.alsolution.chatwoot',
-      permissions: ['android.permission.CAMERA', 'android.permission.RECORD_AUDIO'],
-      googleServicesFile: './google-services.json',
-      intentFilters: [
-        {
-          action: 'VIEW',
-          autoVerify: true,
-          data: [
-            {
-              scheme: 'https',
-              host: 'saas.alsolution.net.br',
-              pathPrefix: '/app/accounts/',
-              pathPattern: '/*/conversations/*',
-            },
-          ],
-          category: ['BROWSABLE', 'DEFAULT'],
-        },
-      ],
+      googleServicesFile: googlePath, // Caminho absoluto corrigido
     },
     extra: {
       eas: {
@@ -58,22 +33,18 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
     plugins: [
       'expo-font',
-      ['react-native-permissions', { iosPermissions: ['Camera', 'PhotoLibrary', 'MediaLibrary'] }],
       '@react-native-firebase/app',
       '@react-native-firebase/messaging',
       [
         'expo-build-properties',
         {
           android: {
-            minSdkVersion: 24,
             compileSdkVersion: 34,
             targetSdkVersion: 34,
-            enableProguardInReleaseBuilds: true,
+            buildToolsVersion: "34.0.0",
           },
-          ios: { useFrameworks: 'static' },
         },
       ],
     ],
-    androidNavigationBar: { backgroundColor: '#ffffff' },
   };
 };
