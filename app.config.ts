d085361e5d51f5366b5efd2af3e0dc036c792a2a
alsolution-dev <notifica@alsolution.net.br>
2026-01-30 13:43:10 -0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,31 +2,17 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   return {
+    ...config,
     name: 'AL-Solution Chat',
     slug: 'alsolution-chat',
     version: '4.3.13',
-    orientation: 'portrait',
-    icon: './assets/icon.png',
-    userInterfaceStyle: 'light',
-    newArchEnabled: false,
-    scheme: 'alsolutionapp',
-    splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
-    },
     ios: {
       bundleIdentifier: 'br.net.alsolution.chatwoot',
       googleServicesFile: './google-services.json',
     },
     android: {
       package: 'br.net.alsolution.chatwoot',
-      // Apontamos para o local onde vamos injetar o arquivo manualmente
       googleServicesFile: './google-services.json',
-      adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#ffffff',
-      },
     },
     extra: {
       eas: { projectId: "daec7403-6417-4afa-be86-18ead645d294" },
